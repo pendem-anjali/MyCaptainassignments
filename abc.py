873da3af1a51de75a38ac6de37964e1201831e07
pendem-anjali <109286738+pendem-anjali@users.noreply.github.com>
2022-08-08 21:57:07 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,5 @@
+filename = input("Input the filename : abc.py")
+Input the filename : abc.py
+>>> f_extens = filename.split(".")
+>>> print ("The extension of file is :python ")
+The extension of file is :python
